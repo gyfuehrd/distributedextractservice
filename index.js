f08f617524c83fd0db954c52da27cfb94376589b
gyfuehrd <gs1pankhaha@hotmail.com>
2024-05-10 15:17:38 +0700

    test task

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const dropWhile = (arr, func) => {
-  while (arr.length > 0 && !func(arr[0])) arr = arr.slice(1);
-  return arr;
-};
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack(0, [], 0);
+  return result;
+  function backtrack(start, current, sum) {
+    if (sum === target) {
+      result.push([...current]);
+      return;
+    }
+    if (sum > target || start === candidates.length) return;
+    for (let i = start; i < candidates.length; i++) {
+      current.push(candidates[i]);
+      backtrack(i, current, sum + candidates[i]);
+      current.pop();
+    }
+  }
+}
